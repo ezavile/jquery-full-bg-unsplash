995e47d7075fdf6d7e6dcda2588c23b18e04e7bb
Edgar Zavala <ezavile@gmail.com>
2018-01-19 20:06:23 -0600

    chore: add jquery env on eslint configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-var sayHi = 'Hi';
+/**
+ * Full Bg Unsplash
+ * @version 0.0.0-development
+ * @author Edgar Zavala
+ * @license The MIT License (MIT)
+ */
+
+(function( $ ) {
+  $.fn.showLinkLocation = function() {
+    this.filter('a').append(function() {
+      return '(' + this.href + ')';
+    });
+    return this;
+  };
+}(jQuery));
