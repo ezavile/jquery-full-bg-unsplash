44cedf59cbf72e8f1d2bbad5f885efd4b5cd4ab0
Edgar Zavala <ezavile@gmail.com>
2018-01-19 20:27:46 -0600

    test: add mocha and jsdom to test jquery stuff

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,14 @@
 var expect = require('chai').expect;
+var jsdom = require('jsdom');
+
+var dom = new jsdom.JSDOM('<html><body><a href="page.html">Foo</a></body></html>');
+var $ = global.jQuery = require('jquery')(dom.window);
+
+require('../src');
 
 describe('jq-full-bg-unsplash', function() {
-  it('should be true', function() {
-    expect(true).to.be.true;
+  it('should have the location', function() {
+    $('a').showLinkLocation();
+    expect($('a').text()).to.be.equal('Foo(page.html)');
   });
 });
