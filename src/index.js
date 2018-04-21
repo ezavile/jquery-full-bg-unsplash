6f4edd9a1a92c3afb01fe056348013e872537894
ezavile <ezavile@gmail.com>
2018-04-21 15:04:10 -0500

    add jquery env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-var sayHi = 'Hola mundo';
+/**
+ * jquery-full-bg-unsplash
+ * @version 0.1.0-development
+ * @author Edgar Zavala
+ * @license The MIT License (MIT)
+*/
+
+(function($) {
+  $.fn.setRed = function() {
+    this.css('background-color', 'red');
+    return this;
+  };
+})(jQuery);
