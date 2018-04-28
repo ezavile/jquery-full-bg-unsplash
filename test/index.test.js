e766505489818ab66316840ccb9f12eff27ec1ed
ezavile <ezavile@gmail.com>
2018-04-28 11:50:03 -0500

    add jsdom to test jquery stuff

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,16 @@
 var expect = require('chai').expect;
+var jsdom = require('jsdom');
+
+var dom = new jsdom.JSDOM('<html><body><div id="fake" style="background-color:blue"></div></body></html>');
+var $ = global.jQuery = require('jquery')(dom.window);
+
+require('../src');
 
 describe('jquery-full-bg-unsplash', function() {
-  it('should be true', function() {
-    expect(true).to.be.true;
+  it('should set the background from blue to red', function() {
+    var $fakeDiv = $('#fake');
+    expect($fakeDiv.css('background-color')).to.be.equal('blue');
+    $fakeDiv.setRed();
+    expect($fakeDiv.css('background-color')).to.be.equal('red');
   });
 });
