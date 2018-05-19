22e92185100f4980e2f14a8aca41ba24bffdacf7
ezavile <ezavile@gmail.com>
2018-05-19 11:09:23 -0500

    add functionality to get a random photo

--- src/index.js
+++ src/index.js
@@ -6,8 +6,48 @@
 */
 
 (function($) {
-  $.fn.setRed = function() {
-    this.css('background-color', 'red');
+  function FullBgUnsplash() {}
+
+  FullBgUnsplash.prototype.setup = function(clientId) {
+    this.clientId = clientId;
+
+    $.ajaxSetup({
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Client-ID ' + clientId);
+      }
+    });
+  };
+
+  $.fn.fullBgUnsplash = function(options) {
+    options = options || {};
+
+    this.css({
+      height: '100vh',
+      minHeight: options.minHeight || '800px',
+      width: '100%',
+      backgroundSize: options.backgroundSize || 'cover',
+      backgroundPosition: options.backgroundPosition || 'center',
+      backgroundColor: options.backgroundColor || 'black',
+    });
+
+    var self = this;
+
+    $.ajax({
+      url: 'https://api.unsplash.com/photos/random/',
+      data: {
+        orientation: 'landscape',
+      }
+    })
+      .done(function(photo) {
+        self.css('backgroundImage', 'url(' + photo.urls.regular + ')');
+      })
+      .fail(function() {
+        self.css('backgroundImage', 'url(' + options.backgroundImage + ')');
+      });
+
     return this;
   };
+
+
+  window.fullBgUnsplash = new FullBgUnsplash();
 })(jQuery);
